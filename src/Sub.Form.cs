6a39c481d7835fcda000e303c90ce03d74a7e1df
osawa-koki <osawa-koki@example.com>
2023-03-14 14:30:47 +0900

    フォーム修正した。

--- src/Sub.Form.cs
+++ src/Sub.Form.cs
@@ -1,15 +1,5 @@
 ﻿using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
-using iText.Kernel.Pdf.Canvas.Parser;
-using iText.Kernel.Pdf.Canvas;
-using iText.Layout;
-using iText.Layout.Element;
-using iText.Kernel.Font;
-using iText.Layout.Properties;
-using iText.Layout.Renderer;
-using iText.Kernel.Pdf.Canvas.Parser.Listener;
-using iText.IO.Font.Constants;
-using System.Text.RegularExpressions;
 using iText.Forms;
 using iText.Forms.Fields;
 
@@ -21,30 +11,30 @@ public static partial class Program
     string inputFile = "input.pdf";
     string outputFile = "output.pdf";
 
-    // PDFドキュメントを作成する
-    PdfDocument pdfDoc = new PdfDocument(new PdfWriter(inputFile));
+    {
+      // PDFドキュメントを作成する
+      PdfDocument pdfDoc = new PdfDocument(new PdfWriter(inputFile));
 
-    // ページを追加する
-    PdfPage page = pdfDoc.AddNewPage();
-    PdfCanvas canvas = new PdfCanvas(page);
+      // フォームフィールドを追加する
+      PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+      PdfTextFormField field = PdfFormField.CreateText(pdfDoc, new Rectangle(100, 700, 100, 20), "field_name", "");
+      form.AddField(field);
 
-    // フォームフィールドを追加する
-    PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, true);
-    PdfTextFormField field = PdfFormField.CreateText(pdfDoc, new Rectangle(100, 700, 100, 20), "field_name", "");
-    form.AddField(field);
+      // ドキュメントを閉じる
+      pdfDoc.Close();
+    }
 
-    // ドキュメントを閉じる
-    pdfDoc.Close();
+    {
+      // 作成したPDFドキュメントを開く
+      PdfDocument pdfDoc = new PdfDocument(new PdfReader(inputFile), new PdfWriter(outputFile));
 
-    // 作成したPDFドキュメントを開く
-    PdfDocument pdfDoc2 = new PdfDocument(new PdfReader(inputFile), new PdfWriter(outputFile));
+      // フォームフィールドに値を設定する
+      PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+      PdfFormField field = form.GetField("field_name");
+      field.SetValue("new_field_value");
 
-    // フォームフィールドに値を設定する
-    PdfAcroForm form2 = PdfAcroForm.GetAcroForm(pdfDoc2, true);
-    PdfFormField field2 = form2.GetField("field_name");
-    field2.SetValue("field_value");
-
-    // ドキュメントを閉じる
-    pdfDoc2.Close();
+      // ドキュメントを閉じる
+      pdfDoc.Close();
+    }
   }
 }
