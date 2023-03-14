8f0fde7fade76a3f37b3f6e3631f0deae457fee1
osawa-koki <osawa-koki@example.com>
2023-03-14 12:48:33 +0900

    fst commit;;

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-simple-pdf.cs
---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    // 新しいPDF文書を作成
+    PdfDocument pdfDoc = new(new PdfWriter("HelloWorld.pdf"));
+
+    // ページを作成
+    Document doc = new(pdfDoc);
+
+    // テキストを追加
+    Paragraph para = new("Hello, World!");
+    doc.Add(para);
+
+    // 文書をクローズ
+    doc.Close();
+  }
+}
